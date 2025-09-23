3c0b06e0a26dc4e875e62e9f6118891940087095
Yann de The <ydethe@gmail.com>
2025-09-23 10:53:54 +0200

    Using container

--- src/quizzy/main.py
+++ src/quizzy/main.py
@@ -5,12 +5,22 @@ st.title("Uber pickups in NYC")
 
 # Use the get method since the keys won't be in session_state
 # on the first script run
-if st.session_state.get("answer_01"):
+if st.session_state.get("answer_00"):
     st.session_state["user_answer"] = "0"
-if st.session_state.get("answer_02"):
+
+if st.session_state.get("answer_01"):
     st.session_state["user_answer"] = "1"
 
-st.text_input("Name", key="user_answer")
+if st.session_state.get("answer_02"):
+    st.session_state["user_answer"] = "2"
 
-st.button("Answer 01", key="answer_01")
-st.button("Answer 02", key="answer_02")
+if st.session_state.get("answer_03"):
+    st.session_state["user_answer"] = "3"
+
+
+c = st.container()
+
+c.text_input("Name", key="user_answer")
+
+c.button("Answer 01", key="answer_00")
+c.button("Answer 02", key="answer_01")
